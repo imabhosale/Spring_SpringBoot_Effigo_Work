0830a12735cd4e06c6fe0bc29cbcb51cfdae1c3a
Abhishek Bhosale <abhishekbhosale676@gmail.com>
2025-01-20 11:57:40 +0530

    done with tight and loose coupling with interface

--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
@@ -2,14 +2,19 @@ package com.in28minutes.learn_spring_framework;
 
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.MarioGame;
+import com.in28minutes.learn_spring_framework.game.PacManGame;
 import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 	public static void main(String[] args) {
+		System.out.println("Everything is working here :");
 		
 		//var game = new MarioGame();
-		var game = new SuperContraGame();
+		//var game = new SuperContraGame();
+		var game = new PacManGame();
 		var gameRunner = new GameRunner(game);
+		
+		
 		gameRunner.run();
 	}
 
